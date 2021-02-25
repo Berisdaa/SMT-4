e7836efb82e69af8dfb18b269b1fb64e4ea7f64a
Berisdaa <68280730+Berisdaa@users.noreply.github.com>
2021-02-26 06:58:53 +0700

    Tugas 8 - SMT4 (Persiapan Project Dengan React Js)
    React Js Vid 11 - 20

---
+++ Tugas 8 - SMT4 (Persiapan Project Dengan React Js)/project react-resto/react-resto/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Nav from './Front/Nav';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Belajar React</h1>
+      <h2>Install Snippet Berhasil</h2>
+
+      <Nav />
+
+    </div>
+  );
+}
+
+export default App;
